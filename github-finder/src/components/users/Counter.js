9ee1caaeef531282247f851d046e715f26608c35
matt15y <matt15y@gmail.com>
2020-11-13 19:55:46 -0600

    updated Counter.js

--- github-finder/src/components/users/Counter.js
+++ github-finder/src/components/users/Counter.js
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 
 function Counter() {
 	const [count, setCount] = useState(0);
-	const increment = () => setCount(count + 1);
+	const [isHappy, setIsHappy] = useState(true);
+	const increment = () => {
+		setCount(count + 1);
+		toggleIsHappy();
+	};
+	// const test =
+	const toggleIsHappy = () => {
+		(count + 1) % 2 === 0 ? setIsHappy(true) : setIsHappy(false);
+		console.log(count);
+	};
+
 	return (
 		<div>
 			<h1>{count}</h1>
+			<h1>{isHappy ? ":)" : ":("}</h1>
 			<button onClick={increment}>+1</button>
 		</div>
 	);
