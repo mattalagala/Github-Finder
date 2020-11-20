b86eda3482b402093a617793154c72e542fc7a31
matt15y <matt15y@gmail.com>
2020-11-19 19:35:01 -0600

    added item to ITEMS local data

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -11,6 +11,7 @@ const items = [
 	{ id: 1, name: "Taco Seasoning", price: 2.25, qty: 2 },
 	{ id: 2, name: "Pinto Beans", price: 1.99, qty: 3 },
 	{ id: 3, name: "Sour Cream", price: 3.5, qty: 1 },
+	{ id: 4, name: "Cheessee", price: 4.5, qty: 2 },
 ];
 
 class App extends Component {
